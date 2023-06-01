0c495ba1f0681816f8a430c51c152171cf4ade30
Roger de castro <rogerd023@hotmail.com>
2023-06-01 20:31:41 -0300

    implementação do springdoc

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,43 +19,42 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name ="tb_usuarios")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotNull(message = "O atributo NOME é obrigatório")
 	private String nome;
-	
+
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo Usuario é obrigatório")
 	@Email(message = "O atributo usuario vai receber um e-mail válido")
 	private String usuario;
-	
+
 	@NotBlank(message = "O atributo senha é obrigatório")
 	@Size(min = 8, message = "A senha tem que ter no mínimo 8 caracteres")
 	private String senha;
-	
-	@Size(max = 5000,message = "O link da foto não deve ser maior que 5000 caracteres")
+
+	@Size(max = 5000, message = "O link da foto não deve ser maior que 5000 caracteres")
 	private String foto;
-	
-	@OneToMany(fetch = FetchType.LAZY,mappedBy = "usuario",cascade = CascadeType.REMOVE)
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
-	
-	
 
-	public Usuario(Long id, String nome, String usuario,String senha, String foto) {
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
 		this.foto = foto;
 	}
-	
+
 	public Usuario() {
-		
+
 	}
 
 	public Long getId() {
@@ -104,6 +104,5 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
-	
+
 }
